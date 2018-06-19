ccfddb2af536768d6cc1f8df9a5ff02cd17339d8
Arvind <ari0997@gmail.com>
2018-06-19 19:05:14 +0530

    Added Multisig Events

--- colony/MultiSigEvents.js
+++ colony/MultiSigEvents.js
@@ -2,4 +2,44 @@ import {EventEmitter} from 'events';
 
 class MultiSigEvents extends EventEmitter {
 
+
+    private colonyClient;
+    private ms;
+    static jsonData = null;
+
+    constructor(_colonyClient){
+        super();
+        this.colonyClient = _colonyClient;
+    }
+
+    startSetPayment = async (asyncFunc,taskId, source, amount) => {
+        this.emit('startOperation');
+        try{
+            this.ms = this.colonyClient[asyncFunc].startOperation({taskId, source, amount});
+            this.emit('signatureRequired', this.ms);
+        } catch (err){
+            this.emit('error', err);
+        }
+    };
+
+    signOperation = async (asyncFunc) => {
+        this.emit('signingStart');
+        try{
+            if(MultiSigEvents.jsonData)
+                this.ms = await this.colonyClient[asyncFunc]().restoreOperation(MultiSigEvents.jsonData);
+            this.ms.refresh();
+            this.ms.sign();
+            MultiSigEvents.jsonData = ms.toJSON();
+            this.emit('signingEnd', ms.toJSON());
+        } catch(err){
+            this.emit('error', err);
+        }
+    };
+
+    finalizeOperations = async () => {
+        const {success} = await this.ms.send();
+        this.emit('endOperation', success);
+    }
 }
+
+module.exports = {MultiSigEvents};
--- colony/index.js
+++ colony/index.js
@@ -1,2 +1,3 @@
 export {createNewColony, getColony} from './colony';
 export {createNewTask, modifyTask, getCreatedTask, rateTask} from './task';
+export {MultiSigEvents} from './MultiSigEvents';
